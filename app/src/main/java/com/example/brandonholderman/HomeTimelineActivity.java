0035b5cb7c69ba93e05a63886fdc5e1cd901d1ca
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-09-16 14:39:02 -0700

    displaying listView of tweets

--- app/src/main/java/com/example/brandonholderman/HomeTimelineActivity.java
+++ app/src/main/java/com/example/brandonholderman/HomeTimelineActivity.java
@@ -3,6 +3,8 @@ package com.example.brandonholderman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.brandonholderman.twitterclient.R;
 
@@ -14,16 +16,28 @@ import Model.BHTweet;
 public class HomeTimelineActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeTimelineActivity";
+    private ListView tweetsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_timeline);
 
+        setupListView();
+
         ArrayList<BHTweet> allTweets = BHJson.getTweets(this, true);
 
         for (BHTweet tweet : allTweets) {
             Log.d(TAG, "Tweet Text: " + tweet.text);
         }
+    }
+
+    private void setupListView() {
+        tweetsListView = (ListView) findViewById(R.id.tweet_list_view);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        tweetsListView.setAdapter(adapter);
+        ArrayList<BHTweet> showTweets = BHJson.getTweets(this, true);
+        adapter.addAll(showTweets);
+        adapter.notifyDataSetChanged();
     }
 }
