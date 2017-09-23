90f120cfa102e447fbf7286cc36be8689c921065
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-09-23 16:44:41 -0700

    added custom actionBar working on adding logic to provide logout functionality to button

--- app/src/main/java/com/example/brandonholderman/HomeTimelineActivity.java
+++ app/src/main/java/com/example/brandonholderman/HomeTimelineActivity.java
@@ -3,6 +3,7 @@ package com.example.brandonholderman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +36,9 @@ public class HomeTimelineActivity extends AppCompatActivity implements AdapterVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_timeline);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.am_action_bar);
+        setSupportActionBar(toolbar);
 
         Twitter.initialize(this);
         TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
